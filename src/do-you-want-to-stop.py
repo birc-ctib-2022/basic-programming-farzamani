01d0d512a174782e67ac2c8ca394cab5e73ce2f1
Farhad Zamani <zmnfrhd@gmail.com>
2022-09-06 14:59:57 +0200

    add hello-n.py and do-you-want-to-stop.py

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,3 +1,11 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+
+n = "no"
+
+while n != "yes":
+    n = str(input('Do you want to stop? '))
+    if n == "yes":
+        break
+    
